c9df8b18a47bfac31beed44306b1dd375d96fed7
deoxxa <deoxxa@fknsrs.biz>
2013-02-01 15:32:33 +0900

    added -CA options to valid-client.sh

--- valid-client.sh
+++ valid-client.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 openssl genrsa -out valid-client.key 1024
 openssl req -new -key valid-client.key -out valid-client.csr -config valid-client.cnf
-openssl x509 -req -in valid-client.csr -signkey server.key -out valid-client.crt 
+openssl x509 -req -in valid-client.csr -signkey valid-client.key -CA server.crt -CAkey server.key -CAcreateserial -out valid-client.crt
 openssl pkcs12 -export -in valid-client.crt -inkey valid-client.key -certfile valid-client.crt -out valid-client.pfx
 openssl x509 -noout -text -in valid-client.crt
