b8637b8caf17320e2f5b5d28725abbb84626dae6
Naomi Kyoto <nakyoto@gmail.com>
2013-02-01 00:40:54 -0600

    Add brides.ru invalid-client cert generator

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 rm *.{key,csr,crt,pfx}
 printf "\n\nSERVER\n"
 ./server.sh
+
 printf "\n\nVALID CLIENT\n"
 ./valid-client.sh
+
+printf "\n\nINVALID CLIENT\n"
+./invalid-client.sh
--- valid-client.sh
+++ valid-client.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 openssl genrsa -out valid-client.key 1024
 openssl req -new -key valid-client.key -out valid-client.csr -config valid-client.cnf
 openssl x509 -req -in valid-client.csr -signkey valid-client.key -CA server.crt -CAkey server.key -CAcreateserial -out valid-client.crt
