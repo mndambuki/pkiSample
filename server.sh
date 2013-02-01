fa337f37687b4c739f1d28fe335ff5ddf0c67686
Naomi Kyoto <nakyoto@gmail.com>
2013-02-01 00:05:27 -0600

    Do things more the deoxxa way

--- server.sh
+++ server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 openssl genrsa -out server.key 1024
-openssl req -new -x509 -key server.key -out server.crt -config server.cnf
+openssl req -new -key server.key -out server.csr -config server.cnf
+openssl x509 -req -in server.csr -signkey server.key -out server.crt 
+openssl pkcs12 -export -in server.crt -inkey server.key -certfile server.crt -out server.pfx -config server.cnf
 openssl x509 -noout -text -in server.crt
